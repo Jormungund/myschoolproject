20672d8d6f3c71840fec26ef0d18dec4d8195634
Isaac Worthen <Jormungund@users.noreply.github.com>
2016-09-26 14:12:36 -0600

    Add files via upload

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-	
+
 	$("#myinfo").hide();
-	
+
 	$("#aboutme").click(function() {
 		$("#myinfo").fadeToggle(500);
 	});
 
+	$(".mousechange").mouseenter (function() {
+		$(this).css("font-size", "2.5em");
+		$(this).css("color", "#0000FF");
+	});
+	
+	$(".mousechange").mouseleave (function() {
+		$(this).css("font-size", "2em");
+		$(this).css("color", "#000000");
+	});
 });
+
